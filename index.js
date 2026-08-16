314146df34ed7584514d5e2d39639a55e2df1001
Robert Hughes <rjh2869@gmail.com>
2026-08-16 12:32:20 -0500

    completed isColdAndHot function

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ export function shareLastDigit(num1, num2) {
  * isColdAndHot(10, 50); // false
  */
 export function isColdAndHot(temp1, temp2) {
-  /* TODO */
+  return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
 }
 
 /**
